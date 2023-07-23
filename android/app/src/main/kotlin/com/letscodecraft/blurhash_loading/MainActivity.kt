3e531cc07e2dd43a3395d54f382a9293a6387354
Ravi <ravi.nipunasewa@gmail.com>
2023-07-23 13:22:26 +0545

    project setup

---
+++ android/app/src/main/kotlin/com/letscodecraft/blurhash_loading/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.letscodecraft.blurhash_loading
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
